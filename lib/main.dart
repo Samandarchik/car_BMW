e02fe2d184d49f83b4010c9883280a44f052fe56
Samandar <samandarik5@gmail.com>
2024-10-14 22:49:24 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .5,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    width: double.infinity,
+                    'images/Image.jpeg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.black, // Transparent top part
+                          Colors.black.withOpacity(.7), // Transparent top part
+                          Colors.black.withOpacity(.5), // Transparent top part
+                          Colors.transparent, // Transparent top part
+                          Colors.transparent, // Bottom part
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      right: 0,
+                      left: 0,
+                      bottom: 20,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "images/logo.png",
+                            width: 70,
+                            fit: BoxFit.fill,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            '   "The ultimate\n driving machine."',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w900),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              height: 48,
+              width: MediaQuery.of(context).size.width * .66,
+              decoration: BoxDecoration(
+                  color: Color(0xFF0C446B),
+                  borderRadius: BorderRadius.circular(40)),
+              child: Center(
+                child: Text(
+                  "Log in for free",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+            ),
+            ConteinerPress(
+              text: "Google",
+            ),
+            ConteinerPress(
+              text: "Fecebook",
+            ),
+            ConteinerPress(
+              text: "Apple",
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?   ",
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "Don't have an account?   ",
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class ConteinerPress extends StatelessWidget {
+  final String text;
+
+  const ConteinerPress({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      height: 48,
+      width: MediaQuery.of(context).size.width * .66,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(40)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Image.asset("images/$text.png"),
+          ),
+          Text(
+            "Continue with $text",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          SizedBox()
+        ],
+      ),
+    );
+  }
+}
+// Row(
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.only(left: 12.0,right: 200),
+//             child: Image.asset("images/Google.png"),
+//           ),
+//           Text(
+//             "Log in for free",
+//             style: TextStyle(
+//                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+//           ),
+//         ],
+//       ),
